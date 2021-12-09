a925035dcbdde20f1dea7113885b5a539556b0ea
Evgeniy Devyatkin <mail@edevyatkin.com>
2021-12-09 09:38:43 +0300

    AoC 2021 Day 9

---
+++ AdventOfCode2021/Day9.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021 {
+    public class Day9 {
+        public static async Task Main(string[] args) {
+            var input = await AocHelper.FetchInputAsync(2021, 9);
+
+            var flow = new int[input.Length][];
+            for (var index = 0; index < input.Length; index++) {
+                var s = input[index];
+                flow[index] = s.Select(c => c - '0').ToArray();
+            }
+
+            // PART 1
+            var lowPoints = new HashSet<(int X, int Y)>();
+            int result1 = 0;
+            for (var i = 0; i < flow.Length; i++) {
+                for (int j = 0; j < flow[0].Length; j++) {
+                    int pos = flow[i][j];
+                    if (pos == 9)
+                        continue;
+                    if (i > 0 && flow[i-1][j] < pos 
+                        || i < flow.Length-1 && flow[i+1][j] < pos
+                        || j > 0 && flow[i][j-1] < pos 
+                        || j < flow[0].Length-1 && flow[i][j+1] < pos)
+                        continue;
+                    result1 += pos + 1;
+                    lowPoints.Add((i, j));
+                }
+            }
+            Console.WriteLine(result1);
+            
+            // PART 2
+            int result2 = 0;
+            var areas = lowPoints
+                .Select(low => CalculateBasinArea(flow, low.X, low.Y, new HashSet<(int, int)>()))
+                .ToList();
+
+            int CalculateBasinArea(int[][] arr, int i, int j, HashSet<(int,int)> visited) { 
+                if (i < 0 || i == arr.Length || j < 0 || j == arr[0].Length 
+                     || arr[i][j] == 9 || visited.Contains((i,j)))
+                    return 0;
+                visited.Add((i, j));
+                return 1 + 
+                    CalculateBasinArea(arr, i - 1, j, visited) +
+                    CalculateBasinArea(arr, i + 1, j, visited) +
+                    CalculateBasinArea(arr, i, j - 1, visited) +
+                    CalculateBasinArea(arr, i, j + 1, visited);
+            }
+
+            result2 = areas.OrderByDescending(s => s).Take(3).Aggregate(1, (x, y) => x * y);
+            
+            Console.WriteLine(result2);
+        }
+    }
+}
