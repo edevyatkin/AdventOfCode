e40e2082964018a25f8acccc37cab9c8dfeb5de5
Evgeniy Devyatkin <mail@edevyatkin.com>
2021-12-17 15:57:00 +0300

    AoC 2021 Day 17

---
+++ AdventOfCode2021/Day17.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021; 
+
+public class Day17 {
+    public static async Task Main(string[] args) {
+        var input = await AocHelper.FetchInputAsync(2021, 17);
+
+        var sp = input[0].Split(' ')[2..];
+        var xCoord = sp[0][2..^1].Split("..").Select(int.Parse).ToArray();
+        var yCoord = sp[1][2..].Split("..").Select(int.Parse).ToArray();
+
+        // PART 1
+        int val = -(yCoord[0] + 1);
+        int result1 = val * (val + 1) / 2;
+        Console.WriteLine(result1);
+
+        // PART 2
+        int result2 = 0;
+        for (int iv = 0; iv <= xCoord[1]; iv++) {
+            for (int jv = yCoord[0]; jv <= -(yCoord[0]+1); jv++) {
+                int curIv = iv;
+                int curJv = jv;
+                int i = 0;
+                int j = 0;
+                while (i <= xCoord[1] && j >= yCoord[0]) {
+                    if (i >= xCoord[0] && i <= xCoord[1] && j >= yCoord[0] && j <= yCoord[1]) {
+                        result2++;
+                        break;
+                    }
+                    i += curIv;
+                    j += curJv;
+                    if (curIv > 0)
+                        curIv--;
+                    curJv--;
+                }
+            }
+        }
+        Console.WriteLine(result2);
+    }
+}
