763eb0ec01d60c64005530ddb4ed77bfd67834a5
Evgeniy Devyatkin <mail@edevyatkin.com>
2022-11-22 19:47:33 +0300

    If a session_key file doesn't exist show a description how to create it properly

--- AdventOfCodeClient/AocHelper.cs
+++ AdventOfCodeClient/AocHelper.cs
@@ -6,7 +6,12 @@ namespace AdventOfCodeClient;
 public static class AocHelper {
     private const string SessionKeyFile = "session_key";
 
-    public static async Task<string[]> FetchInputAsync(int year, int day) {
+    public static async Task<string[]> FetchInputAsync(int year, int day)
+    {
+        if (!File.Exists(SessionKeyFile))
+            throw new FileNotFoundException("Please create a 'session_key' file with your AoC key into AdventOfCodeClient project folder. " +
+                                            "Your can get the session key from cookies of adventofcode.com site after logging in.");
+        
         var sessionKey = await File.ReadAllTextAsync(SessionKeyFile);
         var aocClient = new AocClient(sessionKey.Trim());
         return await aocClient.FetchInputAsync(year, day);
