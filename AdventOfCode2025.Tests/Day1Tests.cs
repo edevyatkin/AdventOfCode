47be498b351dedb3b7033a4727263db63070a382
Evgeniy Devyatkin <mail@edevyatkin.com>
2025-12-01 11:32:24 +0300

    AoC 2025 Day 1

--- AdventOfCode2025.Tests/Day1Tests.cs
+++ AdventOfCode2025.Tests/Day1Tests.cs
@@ -4,13 +4,17 @@ public class Day1Tests
 {
     [Theory]
     [InlineData(new [] {
-        "3   4",
-        "4   3",
-        "2   5",
-        "1   3",
-        "3   9",
-        "3   3"
-    }, 11)]
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"
+    }, 3)]
     public void Part1Test(string[] input, int result)
     {
         Assert.Equal(result, new Day1().Solve(input).Part1);
@@ -18,13 +22,56 @@ public class Day1Tests
     
     [Theory]
     [InlineData(new [] {
-        "3   4",
-        "4   3",
-        "2   5",
-        "1   3",
-        "3   9",
-        "3   3"
-    }, 31)]
+        "L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"
+    }, 6)]
+    [InlineData(new [] {
+        "L49"
+    }, 0)]
+    [InlineData(new [] {
+        "L50"
+    }, 1)]
+    [InlineData(new [] {
+        "L51"
+    }, 1)]
+    [InlineData(new [] {
+        "L100"
+    }, 1)]
+    [InlineData(new [] {
+        "L149"
+    }, 1)]
+    [InlineData(new [] {
+        "L150"
+    }, 2)]
+    [InlineData(new [] {
+        "L151"
+    }, 2)]
+    [InlineData(new [] {
+        "R49"
+    }, 0)]
+    [InlineData(new [] {
+        "R50"
+    }, 1)]
+    [InlineData(new [] {
+        "R51"
+    }, 1)]
+    [InlineData(new [] {
+        "R100"
+    }, 1)]
+    [InlineData(new [] {
+        "R150"
+    }, 2)]
+    [InlineData(new [] {
+        "R1000"
+    }, 10)]
     public void Part2Test(string[] input, int result)
     {
         Assert.Equal(result, new Day1().Solve(input).Part2);
--- AdventOfCode2025/Day1.cs
+++ AdventOfCode2025/Day1.cs
@@ -9,6 +9,24 @@ public class Day1 : IAocDay
     {
         var result1 = 0;
         var result2 = 0;
+        var point = 50;
+        foreach (var line in input)
+        {
+            var c = int.Parse(line[1..]);
+            if (line[0] == 'L')
+            {
+                if (point - c <= 0)
+                    result2 += -(point - c) / 100 + (point > 0 ? 1 : 0);
+            }
+            else
+            {
+                if (point + c >= 100)
+                    result2 += (point + c) / 100;
+            }
+            point = (point + 1000 + (line[0] == 'L' ? -1 : 1) * c) % 100;
+            if (point == 0)
+                result1++;
+        }
         return new AocDayResult(result1, result2);
     }
 }
